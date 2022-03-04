4226145c374439d42aadc49910c3deb122210fbd
Christian <86637723+SquareHammer89@users.noreply.github.com>
2022-03-04 11:24:12 -0800

    Delete date.js

--- fire-web-app/static/script/date.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
- 
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-options.timeZoneName = 'short';
- 
-var now = today.toLocaleString('en-US', options);
-console.log(now);
