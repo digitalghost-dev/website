0c8875fdb0994eb49bd45dbb7a71690a4347ba01
Christian <86637723+SquareHammer89@users.noreply.github.com>
2022-03-06 16:11:20 -0800

    Update popup.js

--- fire-web-app/static/scripts/popup.js
+++ fire-web-app/static/scripts/popup.js
@@ -1,7 +1,12 @@
-var today = new Date();
- 
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-options.timeZoneName = 'short';
- 
-var now = today.toLocaleString('en-US', options);
-console.log(now);
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
