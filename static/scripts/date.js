12041e4124a3015fa2e33c0f69e78df4a5dcdbb7
Christian <86637723+SquareHammer89@users.noreply.github.com>
2022-05-03 10:57:18 -0700

    removing the timezone part of the date

--- static/scripts/date.js
+++ static/scripts/date.js
@@ -1,7 +1,6 @@
 var today = new Date();
  
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-options.timeZoneName = 'short';
  
 var now = today.toLocaleString('en-US', options);
 console.log(now);
